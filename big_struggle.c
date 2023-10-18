c6847ba2c31ffd69e035d6c13d61a163fd1bd6fb
ShalitaCollins <nyarwayashalitacollin@gmail.com>
2023-10-18 15:18:12 +0300

    big struggle

---
+++ big_struggle.c
@@ -0,0 +1,28 @@
+// big_struggle.c
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    int count = 0;
+    va_list args;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            format++;
+            count++;
+        }
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}        
